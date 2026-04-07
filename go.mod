9e456b90ae4e84046379c41856fc3f535ae3a418
joshmg <joshmg.codes@gmail.com>
2026-04-07 14:15:02 -0500

    Create new server multiplexer

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/joshmgreen/chirpy
+
+go 1.24.6
---
+++ main.go
@@ -0,0 +1,11 @@
+package mai
+
+import (
+	"fmt"
+)
+
+func main() {
+	server := NewServeMux() * ServeMux
+
+	fmt.Println(server)
+}
