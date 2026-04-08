6a1a2aa901b8155ba9ce877ed540a5b242969e4b
joshmg <joshmg.codes@gmail.com>
2026-04-08 15:08:17 -0500

    implement simple file server

--- main.go
+++ main.go
@@ -5,8 +5,16 @@ import (
 	"net/http"
 )
 
+type homeHandler struct{}
+
+func (h *homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome home!"))
+}
+
 func main() {
 	mux := http.NewServeMux()
+
+	mux.Handle("/", http.FileServer(http.Dir(".")))
 
 	server := http.Server{
 		Addr:    ":8080",
