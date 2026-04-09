47e4b58abb6162ee7214f70aee025830b17198bb
joshmg <joshmg.codes@gmail.com>
2026-04-09 10:35:22 -0500

    Create handler for heatlh check endpoint

--- main.go
+++ main.go
@@ -10,6 +10,14 @@ func main() {
 
 	mux.Handle("/", http.FileServer(http.Dir(".")))
 
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
